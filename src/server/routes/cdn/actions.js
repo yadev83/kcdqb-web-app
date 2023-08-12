cb1bb7c9c173de4b925298179624029820edd35d
Yanis <yanis.attia@outlook.fr>
2023-08-12 20:58:50 +0200

    cdn /list now scans the /cdn dir for files to dl

--- src/server/routes/cdn/actions.js
+++ src/server/routes/cdn/actions.js
@@ -1,3 +1,13 @@
+import path from 'path'
+import fs from 'fs'
+
 export const getDownloadablesList = (req, res) => {
-    return res.status(200).json({message: 'ok'})
+    const downloadablesDir = path.join(process.cwd(), 'cdn')
+
+    fs.promises.readdir(downloadablesDir).then(filenames => {
+        return res.status(200).json(filenames)
+    }).catch(err => {
+        console.error(`ERROR while getDownloadablesList ${err.toString()}`)
+        return res.status(500).json({error: `ERROR while getDownloadablesList ${err.toString()}`})
+    })
 }
