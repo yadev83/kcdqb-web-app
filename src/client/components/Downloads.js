c613462293ba2d4771254564d30a155bdabfd4df
Yanis <yanis.attia@outlook.fr>
2023-08-13 09:56:33 +0200

    Frontend + file stats from /list

--- src/client/components/Downloads.js
+++ src/client/components/Downloads.js
@@ -2,21 +2,28 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import lo from 'lodash'
 
+import { API_URL } from '../utils'
 import { LottieRender, Loading, Error } from '../utils/components'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Row, Table } from 'react-bootstrap'
+
+const SIZE_TYPES = ['o', 'Ko', 'Mo', 'Go']
 
 class Downloads extends Component {
     constructor(props) {
         super(props)
 
+        const newList = lo.cloneDeep(props?.list)
+
         this.state = {
-            list: props?.list || []
+            // Filter to keep only files (might update to handle directories if needed)
+            list: newList?.length ? newList?.filter(file => file?.type && file.type === 'file') : []
         }
     }
 
     componentDidUpdate(prevProps, prevState) {
         if(!lo.isEqual(prevProps.list, this.props.list)) {
-            this.setState({list: lo.cloneDeep(this.props.list)})
+            const newList = lo.cloneDeep(this.props.list)
+            this.setState({list: newList?.length ? newList?.filter(file => file?.type && file.type === 'file') : []})
         }
     }
 
@@ -34,10 +41,43 @@ class Downloads extends Component {
             </Row>
             <Row>
                 <Col xs={12}>
-                    {loadingList ? <Loading /> : errorList ? <Error error={errorList} /> : null}
-                    {list?.length ? <pre>
-                        {JSON.stringify(list, null, 2)}
-                    </pre> : <p style={{width: '100%', textAlign: 'center'}}>
+                    {loadingList ? <Loading /> : errorList ? <Error error={errorList} /> : list?.length ? <Table>
+                        <thead>
+                            <th>Nom du fichier</th>
+                            <th>Taille</th>
+                            <th>Mise en ligne</th>
+                            <th>Dernière mise à jour</th>
+                            <th>Télécharger</th>
+                        </thead>
+                        <tbody>
+                            {list.map((file, index) => {
+                                // Calculate a human readable file size (o, ko, mo, go)
+                                // Automatically adjusts based on how big the file is
+                                let sizeStr = file.size.toString()
+                                let size = file.size, sizeType = 0
+
+                                while(sizeStr.length > 4 && sizeType < 3) {
+                                    size = size / 1024
+                                    sizeType++
+
+                                    sizeStr = Math.trunc(size).toString()
+                                }
+
+                                // Produce human readable create/update dates
+                                const creationDate = new Date(file.created_at).toLocaleString()
+                                const updateDate = new Date(file.updated_at).toLocaleString()
+                                
+                                // Render the file row
+                                return <tr key={index}>
+                                    <td>{file.name}</td>
+                                    <td>{size.toFixed(3)} {SIZE_TYPES[sizeType]}</td>
+                                    <td>{creationDate}</td>
+                                    <td>{updateDate}</td>
+                                    <td><a href={`${API_URL}/cdn/${file.name}`}>Télécharger</a></td>
+                                </tr>
+                            })}
+                        </tbody>
+                    </Table> : <p style={{width: '100%', textAlign: 'center'}}>
                         Aucun fichier n'est disponible au téléchargement sur le serveur
                     </p>}
                 </Col>
--- src/server/routes/cdn/actions.js
+++ src/server/routes/cdn/actions.js
@@ -5,7 +5,20 @@ export const getDownloadablesList = (req, res) => {
     const downloadablesDir = path.join(process.cwd(), 'cdn')
 
     fs.promises.readdir(downloadablesDir).then(filenames => {
-        return res.status(200).json(filenames)
+        return Promise.all(filenames.map(filename => {
+            return fs.promises.stat(path.join(process.cwd(), 'cdn', filename)).then(stats => {
+                return Promise.resolve({
+                    name: filename,
+                    type: stats.isDirectory() ? 'dir' : stats.isFile() ? 'file' : undefined,
+                    size: stats.size,
+                    created_at: stats.mtime,
+                    updated_at: stats.ctime,
+                    last_download: stats.atime
+                })
+            })
+        }))
+    }).then((files) => {
+        return res.status(200).json(files)
     }).catch(err => {
         console.error(`ERROR while getDownloadablesList ${err.toString()}`)
         return res.status(500).json({error: `ERROR while getDownloadablesList ${err.toString()}`})
